81d33d7a28770c607832cf3e1e3d6f8ce49c96ad
thomaslau <thomas_0836@qq.com>
2016-03-26 17:40:23 +0800

    1加入了ueditor

--- public/app/main.js
+++ public/app/main.js
@@ -4,8 +4,18 @@ import $ from 'jquery';
 
 import {imageUploadeInit} from './uploade/ImageUploader';
 
-$('[data-uploader-pricture]').each(function(){
-    imageUploadeInit($(this));
+import {ueditorInit} from './ueditor/UeditorInit';
+
+$(function(){
+
+    $('[data-ueditor]').each( () => {
+        ueditorInit($(this).attr('id'));
+    });
+
+
+    $('[data-uploader-pricture]').each( () => {
+        imageUploadeInit($(this));
+    });
 });
 
 console.log('1');
