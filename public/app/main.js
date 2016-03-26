05fa104923108030a8ac30c1a8ea05df6bacfb16
thomaslau <thomas_0836@qq.com>
2016-03-26 20:34:40 +0800

    1 修复注册事件事的this 的错用

--- public/app/main.js
+++ public/app/main.js
@@ -8,13 +8,13 @@ import {ueditorInit} from './ueditor/UeditorInit';
 
 $(function(){
 
-    $('[data-ueditor]').each( () => {
-        ueditorInit($(this).attr('id'));
+    $('[data-ueditor]').each( (index,t) => {
+        ueditorInit($(t).attr('id'));
     });
 
 
-    $('[data-uploader-pricture]').each( () => {
-        imageUploadeInit($(this));
+    $('[data-uploader-pricture]').each( (index,t) => {
+        imageUploadeInit($(t));
     });
 });
 
